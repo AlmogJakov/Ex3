bf2f0074526eae3772659c03e8e87eb676f4e514
Itay.R <itayrafee@gmail.com>
2021-01-07 09:29:18 +0200

    fix test

--- src/test_RunTime.py
+++ src/test_RunTime.py
@@ -1,10 +1,11 @@
 import json
 import timeit
 import unittest
+from asyncio import sleep
 
 import networkx as nx
 
-
+from src.GraphAlgo import GraphAlgo
 
 
 def load_to_nx(file_name):
@@ -23,20 +24,16 @@ def load_to_nx(file_name):
 def check(file_name):
     print(" * My graph *")
 
-    # ========================
-    # ==== NetworkX graph ====
-    # ========================
-    print(" * networkx graph *")
-    graph_nx = load_to_nx(file_name)
+    ga = GraphAlgo()
+    ga.load_from_json(file_name)
 
     # connected_components function
     start = timeit.default_timer()
-    gcn = list(nx.strongly_connected_components(graph_nx))
+    gc = ga.connected_components()
     stop = timeit.default_timer()
     print('Time for connected_components function: ', stop - start)
 
-
-    max_connected = max(gcn, key=len)
+    max_connected = max(gc, key=len)
     min_id = min(max_connected)
 
     final_list = []
@@ -49,8 +46,33 @@ def check(file_name):
         final_list.append(max_1)
         max_connected.remove(max_1)
 
-    print("min_id=" + str(min_id))
-    print("max_connected=" + str(final_list))
+    # shortest_path function
+    start = timeit.default_timer()
+    for i in final_list:
+        shortest_list.append(ga.shortest_path(min_id, i))
+
+    stop = timeit.default_timer()
+    print('Time for shortest_path function: ', (stop - start) / 5)
+
+    # connected_component function
+    start = timeit.default_timer()
+    for i in final_list:
+        ga.connected_component(i)
+
+    stop = timeit.default_timer()
+    print('Time for connected_component function: ', (stop - start) / 5)
+
+    # ========================
+    # ==== NetworkX graph ====
+    # ========================
+    print("\n * networkx graph *")
+    graph_nx = load_to_nx(file_name)
+
+    # connected_components function
+    start = timeit.default_timer()
+    gcn = list(nx.strongly_connected_components(graph_nx))
+    stop = timeit.default_timer()
+    print('Time for connected_components function: ', stop - start)
 
     # shortest_path function
     start = timeit.default_timer()
@@ -60,7 +82,24 @@ def check(file_name):
     stop = timeit.default_timer()
     print('Time for shortest_path function: ', (stop - start) / 5)
 
-    return
+    b = True
+
+    if not len(gc) == len(gcn):
+        b = False
+
+    for i in range(t):
+        for i1 in shortest_list_nx:
+            d = 0
+            for i2 in range(len(i1) - 1):
+                d += graph_nx[i1[i2]][i1[i2 + 1]]['weight']
+
+            if not 0.0001 > shortest_list[i][0] - d > -0.0001:
+                b = True
+
+    print("\n\nmin_id = " + str(min_id))
+    print("max_connected = " + str(final_list))
+
+    return b
 
 
 class MyTestCase(unittest.TestCase):
